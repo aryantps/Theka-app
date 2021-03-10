8579adb6d7bccf14d8efdfed8e4958fdce40d8a6
aryant <aryantpratapsingh@outlook.com>
2021-03-10 14:41:07 +0530

    made html dynamic

---
+++ frontend/src/data.js
@@ -0,0 +1,74 @@
+//source of products
+
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Bacardi',
+            category: 'Rum',
+            image: '/images/product-1.jpg',
+            price: 1000,
+            brand: 'Bacardi Limited',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality alcohol'
+        },
+        {
+            _id: '2',
+            name: 'Antiquity Blue Whisky',
+            category: 'Whisky',
+            image: '/images/product-2.jpg',
+            price: 1200,
+            brand: 'United Spirits Limited',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality alcohol'
+        },
+        {
+            _id: '3',
+            name: 'Black Dog Gold Reserve Scotch',
+            category: 'Scotch',
+            image: '/images/product-3.jpg',
+            price: 800,
+            brand: 'United Spirits Limited',
+            rating: 4.8,
+            numReviews: 17,
+            description: 'high quality alcohol'
+        },
+        {
+            _id: '4',
+            name: 'Signature Rare Aged Whisky',
+            category: 'Whisky',
+            image: '/images/product-4.jpg',
+            price: 700,
+            brand: 'United Spirits Limited',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality alcohol'
+        },
+        {
+            _id: '5',
+            name: 'Royal Stag Barrel Select Whisky',
+            category: 'Whisky',
+            image: '/images/product-5.jpg',
+            price: 1100,
+            brand: 'Pernod Ricard',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality alcohol'
+        },
+        {
+            _id: '6',
+            name: 'McDowell\'s No.1',
+            category: 'Whisky',
+            image: '/images/product-6.jpg',
+            price: 1500,
+            brand: 'United Spirits Limited',
+            rating: 4.5,
+            numReviews: 15,
+            description: 'high quality alcohol'
+        },
+    ],
+};
+
+export default data;
